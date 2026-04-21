59cb456ba385644c110be626a08b33f269b06e1f
Ryougi Shikii <sumitkumar12182005@gmail.com>
2026-04-21 21:53:37 +0530

    Added Todo list

---
+++ MyCodes/CryptoGraphy/Todo.py
@@ -0,0 +1,30 @@
+'''
+
+1 Substitution Cipher
+-Caesar
+-MonoAlphatic
+
+2 Transposition Cipher
+-RailFence
+-Columnar
+
+3 Hill Cipher
+4 Steganography
+
+5 P-Box and S-Box
+6 Fiestal Network
+
+7 DES
+8 AES
+
+9 Fermat's Little Theoram
+10 Euler's Theoram
+
+11 Coprime(x, y) and gcd(x, y)
+
+12 Chinese Remainder Theoram
+13 Digital Signature
+14 Diffie Hellman Key Exchange
+15 Kerberas
+
+'''
